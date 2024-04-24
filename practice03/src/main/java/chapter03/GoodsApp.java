9cd417cf0d839d5ed995bbbf173c00f1af9ae08b
kimgip <gippeum0102@gmail.com>
2024-04-24 15:49:34 +0900

    Class 정의 필드#complete, Class 메서드#1

--- practice03/src/main/java/chapter03/GoodsApp.java
+++ practice03/src/main/java/chapter03/GoodsApp.java
@@ -3,8 +3,23 @@ package chapter03;
 public class GoodsApp {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Goods camera = new Goods();
+		camera.setName("nikon");
+		camera.setPrice(400000);
+		camera.setCountSold(10);
+		camera.setCountStock(20);
+		
+		camera.showInfo();
+		
+		// 정보은닉(데이터보호)
+//		camera.setPrice(-400000);
+		
+		// static 변수(클래스 변수)
+		Goods goods2 = new Goods();
+		Goods goods4 = new Goods();
+		System.out.println(Goods.countOfGoods);
+		
+		System.out.println(camera.calcDiscountPrice(0.5f));
 	}
 
 }
---
+++ practice03/src/main/java/chapter03/StaticMethod.java
@@ -0,0 +1,8 @@
+package chapter03;
+
+public class StaticMethod {
+	int n;
+	static int m;
+	
+	
+}
