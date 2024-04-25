9d49208d6e2e2f06f02e7f55c9399d4f351b8b9d
kimgip <gippeum0102@gmail.com>
2024-04-25 16:34:07 +0900

    complete

---
+++ practice04/src/main/java/prob03/Unit.java
@@ -0,0 +1,23 @@
+package prob03;
+
+public abstract class Unit {
+	// 현재 위치
+	private int x;
+	private int y;
+	
+	public int getX() {
+		return x;
+	}
+	public void setX(int x) {
+		this.x = x;
+	}
+	public int getY() {
+		return y;
+	}
+	public void setY(int y) {
+		this.y = y;
+	}
+	
+	abstract void move(int x, int y);
+	abstract void stop();
+}
---
+++ practice04/src/main/java/prob05/MyBase.java
@@ -0,0 +1,27 @@
+package prob05;
+
+public class MyBase extends Base {
+	public void service(String state){
+		switch(state){
+		case "낮":
+			day();
+			break;
+		case "오후":
+			afternoon();
+			break;
+		case "밤":
+			night();
+			break;
+		}
+	}
+	
+	private void afternoon() {
+		System.out.println("오후도 낮과 마찬가지로 일해야 합니다.");
+		
+	}
+
+	public void day(){
+		System.out.println("낮에는 열심히 일하자!");
+	}
+	
+}
