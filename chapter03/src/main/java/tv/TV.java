5e3c655ee16929263f30c5e198d9c1d326544bf5
kimgip <gippeum0102@gmail.com>
2024-04-25 15:41:41 +0900

    complete

---
+++ chapter03/src/main/java/tv/TV.java
@@ -0,0 +1,73 @@
+package tv;
+
+public class TV {
+	private int volume;		// range 0 ~ 100 up down으로 범위 초과시 경계값에 머무르기
+	private int channel;	// range 1 ~ 255 up down으로 범위 초과시 라운딩
+	private boolean power;
+	
+	public TV(int channel, int volume, boolean power) {
+		this.channel = channel;
+		this.volume = volume;
+		this.power = power;
+	}
+
+	public void status() {
+		String powerStr;
+		if (power) {
+			powerStr = "on";
+		} else {
+			powerStr = "off";
+		}
+		System.out.printf("TV[power=%s, channel=%d, volume=%d]\n", powerStr, channel, volume);
+		
+	}
+
+	public void power(boolean power) {
+		this.power = power;
+		
+	}
+	
+	public void volume(int volume) {
+		this.volume = volume;
+		
+		if (volume > 100) {
+			this.volume = 100;
+		} else if (volume < 0) {
+			this.volume = 0;
+		}
+		
+	}
+
+	public void volume(boolean volume) {
+		if (volume) {
+			this.volume++;
+		} else {
+			this.volume--;
+		}
+		volume(this.volume);
+		
+	}
+	
+	public void channel(int channel) {
+		this.channel = channel;
+		
+		if (channel > 255) {
+			this.channel = 1;
+		} else if (channel < 1) {
+			this.channel = 255;
+		}
+		
+	}
+
+	public void channel(boolean channel) {
+		if (channel) {
+			this.channel++;
+		} else {
+			this.channel--;
+		}
+		channel(this.channel);
+		
+	}
+	
+	
+}
