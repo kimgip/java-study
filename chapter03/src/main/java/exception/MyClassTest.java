37bf46f533cc36e58581115f9f705b897841b9b4
kimgip <gippeum0102@gmail.com>
2024-04-26 11:23:46 +0900

    exception 단원 설명 complete

---
+++ chapter03/src/main/java/exception/MyClassTest.java
@@ -0,0 +1,18 @@
+package exception;
+
+import java.io.IOException;
+
+public class MyClassTest {
+
+	public static void main(String[] args) {
+		try {
+			new MyClass().danger();
+		} catch (IOException e) {
+//			System.out.println("error:"+e);
+			e.printStackTrace();
+		} catch (MyException e) {
+			System.out.println("error:"+e);
+		}
+	}
+
+}
