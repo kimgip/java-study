4713e373005cc831b9194a6cadf5338a74e7d5ab
kimgip <gippeum0102@gmail.com>
2024-04-29 11:39:45 +0900

    오버로딩을 제네릭 타입 메소드로 변경

--- practice04/src/main/java/prob01/Printer.java
+++ practice04/src/main/java/prob01/Printer.java
@@ -2,19 +2,23 @@ package prob01;
 
 public class Printer {
 
-	public void println(int i) {
-		System.out.println(i);
-	}
+//	public void println(int i) {
+//		System.out.println(i);
+//	}
+//	
+//	public void println(boolean b) {
+//		System.out.println(b);
+//	}
+//	
+//	public void println(double d) {
+//		System.out.println(d);
+//	}
+//	
+//	public void println(String str) {
+//		System.out.println(str);
+//	}
 	
-	public void println(boolean b) {
-		System.out.println(b);
-	}
-	
-	public void println(double d) {
-		System.out.println(d);
-	}
-	
-	public void println(String str) {
-		System.out.println(str);
+	public <T> void println(T t) {
+		System.out.println(t);
 	}
 }
