0ed39459981a1a7a61408e062f25aee464a54807
kimgip <gippeum0102@gmail.com>
2024-05-01 18:56:28 +0900

    NSLookup complete

--- network/src/main/java/util/NSLookup.java
+++ network/src/main/java/util/NSLookup.java
@@ -2,15 +2,32 @@ package util;
 
 import java.net.InetAddress;
 import java.net.UnknownHostException;
+import java.util.Scanner;
 
 public class NSLookup {
 
 	public static void main(String[] args) {
+		Scanner scanner = null;
 		try {
-			InetAddress[] inetAddresses = InetAddress.getAllByName("www.naver.com");
-			
+			scanner = new Scanner(System.in);
+			while(true) {
+				System.out.print("> ");
+				String hostName = scanner.nextLine();
+				
+				hostName = hostName.strip();
+				if("exit".equals(hostName)) {
+					break;
+				}
+				
+				InetAddress[] inetAddresses = InetAddress.getAllByName(hostName);
+				for(InetAddress inetAddress : inetAddresses) {
+					System.out.println(inetAddress.getHostName()+" : "+inetAddress.getHostAddress());
+				}
+			}
 		} catch (UnknownHostException e) {
-			e.printStackTrace();
+			System.out.println("unknown host name");
+		} finally {
+			scanner.close();
 		}
 	}
 
