cd1f1dcde21e8de0877a3d32389805db15732970
kimgip <gippeum0102@gmail.com>
2024-05-03 11:51:49 +0900

    chatClient gui

---
+++ network/src/main/java/chat/gui/ChatWindow.java
@@ -0,0 +1,111 @@
+package chat.gui;
+import java.awt.BorderLayout;
+import java.awt.Button;
+import java.awt.Color;
+import java.awt.Frame;
+import java.awt.Panel;
+import java.awt.TextArea;
+import java.awt.TextField;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class ChatWindow {
+
+	private Frame frame;
+	private Panel pannel;
+	private Button buttonSend;
+	private TextField textField;
+	private TextArea textArea;
+
+	public ChatWindow(String name) {
+		frame = new Frame(name);
+		pannel = new Panel();
+		buttonSend = new Button("Send");
+		textField = new TextField();
+		textArea = new TextArea(30, 80);
+		
+	}
+
+	public void show() {
+		// Button
+		buttonSend.setBackground(Color.GRAY);
+		buttonSend.setForeground(Color.WHITE);
+		buttonSend.addActionListener( new ActionListener() {
+			@Override
+			public void actionPerformed( ActionEvent actionEvent ) {
+				sendMessage();
+			}
+		});
+
+		// Textfield
+		textField.setColumns(80);
+		textField.addKeyListener(new KeyAdapter() {
+			@Override
+			public void keyPressed(KeyEvent e) {
+				char keyCode = e.getKeyChar();
+				if(keyCode == KeyEvent.VK_ENTER) {
+					sendMessage();
+				}
+			}
+		});
+
+		// Pannel
+		pannel.setBackground(Color.LIGHT_GRAY);
+		pannel.add(textField);
+		pannel.add(buttonSend);
+		frame.add(BorderLayout.SOUTH, pannel);
+
+		// TextArea
+		textArea.setEditable(false);
+		frame.add(BorderLayout.CENTER, textArea);
+
+		// Frame
+		frame.addWindowListener(new WindowAdapter() {
+			public void windowClosing(WindowEvent e) {
+				System.exit(0);
+			}
+		});
+		frame.setVisible(true);
+		frame.pack();
+		
+		// IOSTream 받아오기
+		// ChatClientThread 생성
+		
+	}
+	
+	private void sendMessage() {
+		String message = textField.getText();
+		if("".equals(message))
+			return;
+		System.out.println("메세지 보내는 프로토콜:"+message+"<<");
+		
+		textField.setText("");
+		textField.requestFocus();
+	}
+	
+	private void updateTextAread(String message) {
+		textArea.append(message);
+		textArea.append("\n");
+	}
+
+	private class chatClientThread extends Thread {
+		private BufferedReader br;
+		
+		public chatClientThread(BufferedReader br) {
+			this.br = br;
+		}
+		
+		public void run() {
+			while(true) {
+//				String message = br.readline();
+				updateTextAread("마이콜: ");
+			}
+		}
+	}
+}
