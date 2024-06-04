1a76e3b6f6ae82621c5789b7706334785a3fd9e7
kimgip <gippeum0102@gmail.com>
2024-06-04 13:39:21 +0900

    (template method) abstract calculate - 상속

--- designpattern-practice/src/main/java/behavioral/templatemethod/Client.java
+++ designpattern-practice/src/main/java/behavioral/templatemethod/Client.java
@@ -5,6 +5,12 @@ public class Client {
 	public static void main(String[] args) {
 		new Calculator().add();
 		new Calculator().subtract();
+		
+		AbstractCalculate ac = new MultiplyCalculate();
+		ac.templateMethod();
+		
+		ac = new DivideCalculate();
+		ac.templateMethod();
 	}
 
 }
